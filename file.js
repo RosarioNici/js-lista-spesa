5f111deaecd3d714230b58003de0e8a5f1e3d922
Rosario Nici <rosarionici95@gmail.com>
2022-08-31 10:23:20 +0200

    add link for markets

--- file.js
+++ file.js
@@ -10,18 +10,28 @@ Buon lavoro a tutte e a tutti!!*/
 console.log('JS is OK!')
 
 
-let list = ['Uova', 'Pecorino', 'Pepe', 'Guanciale', 'Rigatoni']
+let list = ['Uova', 'Pecorino', 'Pepe', 'Guanciale', 'Rigatoni', 'Pepe rosa']
 
 let markets = ['Esselunga', 'Carrefour', 'Lidl', 'Aldi', 'EuroSpin']
+const linkSupermercati = ['www.esselunga.it', 'www.carrefour.it', 'www.lidl.it', 'www.aldi.it', 'www.eurospin.it']
 
 
 let lap = -1;
 let marketLap = -1;
+let linkLap = -1;
+
+
 while (lap < list.length - 1) {
     lap++;
     console.log(lap, list[lap])
     const messageList = document.getElementById('list')
     messageList.innerHTML += `<li class="py-3 item">${list[lap]}</li>`
+}
+
+
+while (linkLap < linkSupermercati.length - 1) {
+    linkLap++;
+    console.log(linkSupermercati[linkLap])
 }
 
 
@@ -31,5 +41,8 @@ while (marketLap < markets.length - 1) {
     console.log(marketLap, markets[marketLap])
 
     const messageListMarkets = document.getElementById('list-market')
-    messageListMarkets.innerHTML += `<li class="py-3 item">${markets[marketLap]}</li>`
+    messageListMarkets.innerHTML += `<li class="py-3 item"><a href="${linkSupermercati[linkLap]}">${markets[marketLap]}</a></li>`
 }
+
+
+
